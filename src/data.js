f0afe8954504a1c8dbab4fc327bf0bb59dd95da0
abouba03 <abouba1703@yandex.ru>
2023-02-18 22:33:58 +0300

    commit-02

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ export default [
     {
         id: 2,
         title: "Learn Wedding Photography",
-        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        description: "Interested in becoming a wedding photographer?? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
         coverImg: "wedding-photography.png",
         stats: {
